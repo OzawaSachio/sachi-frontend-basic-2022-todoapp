cbbe7a7ea4109dfd00d4980ef4188c6b97afc1fa
Ozawa Sachio <s.ozawa@4nonome.com>
2023-02-12 13:27:39 +0900

    atoms-inputを作り直す

--- src/components/Atoms/Input/index.jsx
+++ src/components/Atoms/Input/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import styled from "styled-components";
+import COLOR from "../../../variables/color";
+import TEXT from "../../../variables/texts";
 
 const Input = () => {
-  return null;
+  return <InputText>S Text</InputText>;
 };
 export default Input;
+
+const InputText = styled.div`
+  color: ${COLOR.LIGHT_GRAY};
+  ${TEXT.S}
+`;
