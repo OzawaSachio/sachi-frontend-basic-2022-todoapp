258800e09d1166de2d8ba0bbca014815779b4ee9
Ozawa Sachio <s.ozawa@4nonome.com>
2023-09-02 13:42:17 +0900

    Pages/MainPageの作成

--- src/components/Pages/MainPage/index.jsx
+++ src/components/Pages/MainPage/index.jsx
@@ -1,2 +1,33 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import COLOR from "../../../variables/color";
+import Title from "../../Atoms/Title";
+import TodoCard from "../../Organisms/TodoCard";
+
+const MainPage = () => {
+  return (
+    <StyledWrapper>
+      <TitleWrapper>
+        <Title></Title>
+      </TitleWrapper>
+      <TodoCardWrapper>
+        <TodoCard></TodoCard>
+      </TodoCardWrapper>
+    </StyledWrapper>
+  );
+};
+
+export default MainPage;
+
+const StyledWrapper = styled.div``;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 20px;
+`;
+
+const TodoCardWrapper = styled.div`
+  background-color: ${COLOR.LIGHT_BLACK};
+`;
