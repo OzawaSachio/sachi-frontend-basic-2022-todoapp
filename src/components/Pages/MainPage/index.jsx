4294ff9d1117c997b5febf66aca337840f2c4ff1
Ozawa Sachio <s.ozawa@4nonome.com>
2023-09-02 14:44:55 +0900

    消去したimport文がやはり必要だったので追加し、その他の部分を整理

--- src/components/Pages/MainPage/index.jsx
+++ src/components/Pages/MainPage/index.jsx
@@ -1,29 +1,27 @@
+import React from "react";
+import styled from "styled-components";
 import COLOR from "../../../variables/color";
 import Title from "../../Atoms/Title";
 import TodoCard from "../../Organisms/TodoCard";
 
 const MainPage = () => {
   return (
-    <StyledWrapper>
+    <>
       <TitleWrapper>
-        <Title></Title>
+        <Title />
       </TitleWrapper>
       <TodoCardWrapper>
-        <TodoCard></TodoCard>
+        <TodoCard />
       </TodoCardWrapper>
-    </StyledWrapper>
+    </>
   );
 };
 
 export default MainPage;
 
-const StyledWrapper = styled.div``;
-
 const TitleWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-bottom: 20px;
+  text-align: center;
+  margin-bottom: 20px;
 `;
 
 const TodoCardWrapper = styled.div`
