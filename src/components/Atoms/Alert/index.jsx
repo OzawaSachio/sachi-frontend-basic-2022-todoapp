55a7e5a11f183e69fd73fd57765094e392e06768
Ozawa Sachio <s.ozawa@4nonome.com>
2023-12-24 15:51:09 +0900

    Alertの画面幅小の際の左右幅20px

--- src/components/Atoms/Alert/index.jsx
+++ src/components/Atoms/Alert/index.jsx
@@ -5,30 +5,39 @@ import TEXT from "../../../variables/texts";
 import BREAKPOINT from "../../../variables/breakpoint";
 
 const Alert = ({ visible, errorText }) => {
-  return <AlertText isActive={visible}> {errorText} </AlertText>;
+  return (
+    <AlertWrapper isActive={visible}>
+      <AlertText> {errorText} </AlertText>
+    </AlertWrapper>
+  );
 };
 
 const AlertText = styled.div`
   color: ${COLOR.WHITE};
   font-size: ${TEXT.S};
-  position: absolute;
-  top: 80px;
-  left: 50%;
-  transition: 0.5s;
-  transform: translateX(-50%) translateY(-40px);
   width: 100%;
   max-width: 400px;
   padding: 10px 20px;
   background-color: ${COLOR.RED};
   border-radius: 4px;
+`;
+
+const AlertWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  top: 80px;
+  left: 50%;
+  transition: 0.5s;
+  transform: translateX(-50%) translateY(-40px);
   opacity: 0;
   ${(props) =>
     props.isActive && `opacity: 1; transform: translateX(-50%) translateY(0%)`};
 
   @media (max-width: ${BREAKPOINT.MEDIUM}) {
+    padding: 0px 20px;
     top: 40px;
-    left: 20px;
-    right: 20px;
   }
 `;
 
